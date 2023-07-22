2d02427d18d47051b146ab16fa7f9c68702bb547
nanerseasac <renan.casaeses@gmail.com>
2023-07-22 19:31:14 -0300

    Finally

--- 03/index.js
+++ 03/index.js
@@ -1,14 +1,7 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
 
 const transformText = (txt) => {
-    let text = ''
-    for(let i = 0;i < txt.length; i++) {
-       const textoLow = txt.toLowerCase()
-       text = textoLow
-       while(text !== text.replace(' ','-')) {
-        text = text.replace(' ','-')
-       }
-    }
+    const text = txt.toLowerCase().split(' ').join('-')
     return text
 }
 
--- 06/index.js
+++ 06/index.js
@@ -3,8 +3,26 @@ const celular = 7199918888;
 const celNum = (num) => {
 	const numStr = num.toString();
 	let ddd = "";
-	if(numStr ===)
-	
+	let firstPart = "";
+	let secondPart = "";
+	const nine = "9";
+	if (numStr.length > 9) {
+		const noDDD = numStr.slice(2);
+		ddd = numStr.slice(0, 2);
+		secondPart = noDDD.slice(-4);
+		firstPart = noDDD.slice(-8, -4);
+	}
+	if (numStr.length === 9 || numStr.length === 8) {
+		firstPart = numStr.slice(-8, -4);
+		secondPart = numStr.slice(-4);
+	}
+
+	if (numStr.length === 10 || numStr.length === 11) {
+		return `(${ddd}) ${nine} ${firstPart}-${secondPart}`;
+	} else if (numStr.length === 8 || numStr.length === 9) {
+		return `${nine} ${firstPart}-${secondPart}`;
+	}
+	return "Número Inválido";
 };
 
-console.log(celular, celNum(celular));
+console.log(celNum(celular));
